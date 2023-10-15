c00f6fc49c25930c4163853657e4af7ad682609f
SuperSTAR-MAN <148005348+SuperSTAR-MAN@users.noreply.github.com>
2023-10-15 13:46:31 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+from dragonstorage import dragon_features(selectedDragon)
+print("Wings Of Fire: Dragon Masters")
+print()
+print(
+  "In a World of Dragons, where it's adventurous, There are many types of Dragons"
+)
+print()
+print(
+  "Types: Mudwings, Skywings, Seawings, Sandwings, Nightwings, Icewings And Rainwings."
+)
+print()
+print(
+  "You are a Dragon master. You will train one of these Dragons and Go one adventures. Just remember one thing, SURVIVE. Good Luck."
+)
+print()
+print("Select a Dragon to get the egg")
+print()
+print("Here are the prompts you need to write:")
+print("1. Mudwing")
+print("2. Skywing")
+print("3. Seawing")
+print("4. Sandwing")
+print("5. Nightwing")
+print("6. Icewing")
+print("7. Rainwing")
+print()
+print("Make sure to put a lowercase letter in the beginning for each response.")
+print()
+dragon_type = input("Dragon > ")
+print()
+print()
